54bb88721eed229a0835334ff24d734bc5f332ad
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-05-08 21:19:28 +0600

    solve problem four

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,34 @@ const resultTwo = concatenateArrays([1, 2], [3, 4], [5]);
 
 console.log(resultOne);
 console.log(resultTwo);
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    console.log(`Make: ${this.make}, year: ${this.year}`);
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    console.log(`Model: ${this.model}`);
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+
+myCar.getInfo();
+myCar.getModel();
