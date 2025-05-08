0b7bc4ca191316cacafbf26713b78f5de436036a
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-05-08 20:35:41 +0600

    solve problem two

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function formatString(input: string, toUpper?: boolean): string {
+/* function formatString(input: string, toUpper?: boolean): string {
   if (toUpper || typeof toUpper === "undefined") {
     return input.toUpperCase();
   } else {
@@ -8,4 +8,25 @@ function formatString(input: string, toUpper?: boolean): string {
 
 console.log(formatString("Hello"));
 console.log(formatString("Hello", false));
-console.log(formatString("Hello", true));
+console.log(formatString("Hello", true)); */
+
+/* Problem 2 */
+
+type FilterByRating = {
+  title: string;
+  rating: number;
+};
+
+function filterByRating(items: FilterByRating[]): FilterByRating[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book b", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+const result = filterByRating(books);
+
+console.log(result);
